ef447eece7887f7c07c06880e6608db21fcbce78
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2023-11-30 16:10:14 -0500

    Add several optional variables of various types.

--- variables.tf
+++ variables.tf
@@ -4,3 +4,60 @@ variable "waypoint_application" {
   description = "Name of the waypoint application, to be used in all resource names."
   type        = string
 }
+
+variable "string_variable" {
+  type        = string
+  description = "An optional string variable."
+  default     = "string"
+}
+
+variable "number_variable" {
+  type        = number
+  description = "An optional number variable."
+  default     = "number"
+}
+
+variable "bool_variable" {
+  type        = bool
+  description = "An optional boolean variable."
+  default     = "bool"
+}
+
+variable "list_string_variable" {
+  type        = list(string)
+  description = "An optional list of string variable."
+  default     = ["list", "of", "string"]
+}
+
+variable "map_string_variable" {
+  type        = map(string)
+  description = "An optional map of string variable."
+  default = {
+    "map" = "string"
+  }
+}
+
+variable "set_string_variable" {
+  type        = set(string)
+  description = "An optional set of string variable."
+  default     = ["set", "of", "string"]
+}
+
+variable "object_variable" {
+  type = object({
+    x = string
+    y = optional(number)
+    z = optional(bool, true)
+  })
+  description = <<EOF
+An optional object variable, with three arguments. The arguments include:
+1. A required string.
+2. An optional number.
+3. An optional bool, which is true by default.
+EOF
+  default = {
+    x = "value"
+    y = 1
+    z = true
+  }
+}
